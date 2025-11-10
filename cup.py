aa3578e7785506b5c7d516b0f32753a165277fb7
NEDIM EFE CANDAS <e2521326@ceng.metu.edu.tr>
2025-11-10 21:28:24 +0300

    add elimination

--- cup.py
+++ cup.py
@@ -1,20 +1,25 @@
+from random import shuffle
+from game import Game
+
 class Cup():
     def __init__(self, teams, type, interval):
         self._teams = teams
-        if (type not in ["ELIMINATION", "GROUP", "LEAGUE"]):
+        if (type not in ["ELIMINATION", "GROUP", "LEAGUE", "ELIMINATION2", "GROUP2", "LEAGUE2"]):
             raise ValueError("Invalid cup type")
         self._type = type
         self._interval = interval
         self._games = {} # gameId -> Game object
 
-        self.observers = set
+        self._initialize_games()
+
+        self.observers = set()
 
     
     def search(self, tname=None, group=None, between=None):
         results = []
 
         nameFilter = lambda team: team.name == tname
-        groupFilter = lambda team: team.group == group
+        groupFilter = lambda team: True # TODO: fix after adding groups
         dateFilter = lambda team: (between[0] <= team.date <= between[1])
 
         filters = []
@@ -22,6 +27,8 @@ class Cup():
         if tname is not None:
             filters.append(nameFilter)
         if group is not None:
+            if self._type not in ["GROUP", "GROUP2"]:
+                raise ValueError("Cannot filter by group in non-group cup type")
             filters.append(groupFilter)
         if between is not None:
             filters.append(dateFilter)
@@ -40,6 +47,7 @@ class Cup():
         
     def standings(self):
         pass
+
     def gametree(self):
         pass
 
@@ -53,3 +61,38 @@ class Cup():
 
     def unwatch(self, obj):
         self.observers.discard(obj) #does not raise error if obj not found
+
+    def _initialize_games(self):
+        if self._type in ["ELIMINATION", "ELIMINATION2"]:
+            self._initialize_elimination()
+        elif self._type in ["GROUP", "GROUP2"]:
+            self._initialize_group()
+        elif self._type in ["LEAGUE", "LEAGUE2"]:
+            self._initialize_league()
+
+    def _initialize_elimination(self): #todo add catalog reference
+        shuffled_teams = self._teams[:]
+        shuffle(shuffled_teams)
+        
+        if len(shuffled_teams) % 2 == 1:
+            bye_team = shuffled_teams.pop()
+            print(f"{bye_team.name} gets a bye to the next round")
+
+        for i in range(0, len(shuffled_teams), 2):
+            if i + 1 < len(shuffled_teams):
+                game = Game(shuffled_teams[i], shuffled_teams[i + 1])
+                self._games[game.id()] = game
+
+        if self._type == "ELIMINATION2":
+            for i in range(0, len(shuffled_teams), 2):
+                if i + 1 < len(shuffled_teams):
+                    game = Game(shuffled_teams[i+1], shuffled_teams[i])
+                    self._games[game.id()] = game
+
+
+    def _initialize_group(self):
+        pass
+
+
+    def _initialize_league(self):
+        pass
